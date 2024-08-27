0224906d8b8a89f17e028993ba94f01cc05b8d14
levan1c <laga.manaois.up@phinmaed.com>
2024-08-27 17:28:04 +0800

    initial commit

---
+++ model/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  enrolled: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+export default mongoose.model("users", userSchema);
